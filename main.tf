fa3d6f306a1208de84f22754df0dd4db684bffe4
sbbennett <sbbennett28@outlook.com>
2025-11-19 20:32:06 -0500

    Update main.tf

--- main.tf
+++ main.tf
@@ -86,5 +86,12 @@ resource "azurerm_storage_account" "storage" {
   account_tier             = "Standard"
   account_replication_type = "LRS"
 
+  network_rules {
+    default_action             = "Deny"
+    ip_rules                   = ["100.0.0.1"]
+    virtual_network_subnet_ids = [azurerm_subnet.subnet.id]
+  }
+
   tags = local.tags
 }
+
